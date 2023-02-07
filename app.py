6b7bf7b06499255462221749fcbdde7ca7b7bdd1
raihanjalil10 <raihanjalil10@gmail.com>
2023-02-07 17:22:53 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,request, url_for, redirect, render_template  
+import pickle 
+import pandas as pd  
+
+app = Flask(__name__)
+
+model_file = open('diabetes.pkl', 'rb')
+model = pickle.load(model_file, encoding='bytes')
+
+@app.route('/')
+def index():
+    return render_template('index.html', insurance_cost=0)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text1 = request.form['1']      
+    text2 = request.form['2'] 
+    text3 = request.form['3']
+    text4 = request.form['4']
+    text5 = request.form['5']
+    text6 = request.form['6']
+    text7 = request.form['7']
+    text8 = request.form['8']
+ 
+    row_df = pd.DataFrame([pd.Series([text1,text2,text3,text4,text5,text6,text7,text8])])  
+    print(row_df)
+    prediction=model.predict_proba(row_df) 
+    output='{0:.{1}f}'.format(prediction[0][1], 2)  
+
+    if output>str(0.5):
+        return render_template('index.html',pred='Anda berpeluang terkena diabetes.\nKemungkinan terkena Diabetes adalah {}'.format(output))
+    else:
+        return render_template('index.html',pred='Anda aman.\n Kemungkinan menderita diabetes adalah {}'.format(output)) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
